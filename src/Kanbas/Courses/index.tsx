6be7714db8508a4466fe766dfb90a3331bc29d53
sravanthinittala <nittala.d@northeastern.edu>
2024-02-19 23:59:24 -0500

    A3

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,76 +1,53 @@
-import React, { useState } from 'react';
-import { courses } from "../../Kanbas/Database";
-import { Navigate, Route, Routes, useParams } from "react-router-dom";
-import { HiMiniBars3 } from "react-icons/hi2";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./index.css";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Breadcrumb from "./Breadcrumb";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import Grades from "./Grades";
-import { useLocation } from 'react-router-dom';
-import './index.css';
+import BlackHeader from "../BlackHeader";
 
 function Courses() {
-    const [isNavVisible, setIsNavVisible] = useState(true); // Initially visible
-    const toggleNav = () => setIsNavVisible(!isNavVisible); // Toggle function
-    const location = useLocation();
-    const pathnames = location.pathname.split('/').filter((x) => x);
+    return (
+        <>
+            <Breadcrumb/>
+            <div className="row">
+                <CourseNavigation />
 
-    // Calculate the start index to slice the array to get only the last two elements
-    const startIndex = pathnames.length > 2 ? pathnames.length - 2 : 0;
-    const lastTwoPathnames = pathnames.slice(startIndex);
+                <div className="d-block d-md-none wd-0-lr-padding">
+                    <BlackHeader/>
+                </div>
 
-  return (
-    <div>
-        <button className="hamburger" onClick={toggleNav}>
-            <HiMiniBars3 /> {/* Assuming HiMiniBars3 is correctly imported */}
-        </button>
-        <nav className="breadcrumb-nav" aria-label="breadcrumb">
-            <ol className="breadcrumb">
-                {lastTwoPathnames.map((name, index) => {
-                // Adjust the index based on the new sliced array for proper routing
-                const routeToIndex = startIndex + index + 1;
-                const routeTo = `/${pathnames.slice(0, routeToIndex).join('/')}`;
-                const isLast = index === lastTwoPathnames.length - 1;
-                const classNames = `breadcrumb-item ${isLast ? 'wd-breadcrumb-header-active' : 'wd-breadcrumb-header'}`;
+                <div className="col-12 col-sm-12 col-md-11 col-lg-11 col-xl-11 col-xxl-11" style={{paddingLeft: "12vw", paddingRight: "12vw"}}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="Home" />} />
+                        <Route path="Home" element={<Home/>} />
+                        <Route path="Modules" element={<Modules/>} />
+                        <Route path="Piazza" element={<h1>Piazza</h1>} />
+                        <Route path="Grades" element={<Grades />} />
+                        <Route path="Assignments" element={<Assignments/>} />
+                        <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>}/>
+                        <Route path="Quizzes" element={<h1>Quizzes</h1>} />
+                        <Route path="People" element={<h1>People</h1>} />
+                        <Route path="Panopto Video" element={<h1>Panopto Video</h1>} />
+                        <Route path="Discussions" element={<h1>Discussions</h1>} />
+                        <Route path="Announcements" element={<h1>Announcements</h1>} />
+                        <Route path="Pages" element={<h1>Pages</h1>} />
+                        <Route path="Files" element={<h1>Files</h1>} />
+                        <Route path="Rubrics" element={<h1>Rubrics</h1>} />
+                        <Route path="Outcomes" element={<h1>Outcomes</h1>} />
+                        <Route path="Collaborations" element={<h1>Collaborations</h1>} />
+                        <Route path="Syllabus" element={<h1>Syllabus</h1>} />
+                        <Route path="Settings" element={<h1>Settings</h1>} />
 
-                return (
-                    <li key={index} className={classNames}>
-
-                    {isLast ? (
-                        <>
-                        {name}
-                        <span className="visually-hidden"> (Current Page)</span>
-                        </>
-                    ) : (
-                        <a href={routeTo}>{name}</a>
-                    )}
-                    </li>
-                );
-                })}
-            </ol>
-        </nav>
-        <hr className="wd-custom-divider" />
-    
-        {isNavVisible && <CourseNavigation className={`course-navigation ${isNavVisible ? 'course-navigation-visible' : ''}`} />}
-      <div>
-        <div
-          className="overflow-y-scroll position-fixed bottom-0 end-0"
-          style={{ left: "320px", top: "50px" }} >
-          <Routes>
-            <Route path="/" element={<Navigate to="Home" />} />
-            <Route path="Home" element={<Home/>} />
-            <Route path="Modules" element={<Modules/>} />
-            <Route path="Piazza" element={<h1>Piazza</h1>} />
-            <Route path="Assignments" element={<Assignments/>} />
-            <Route path="Assignments/:assignmentId" element={<AssignmentEditor/>} />
-            <Route path="Grades" element={<Grades/>} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+                        <Route path="Zoom Meetings" element={<h1>Zoom Meetings</h1>} />
+                        <Route path="Progress Reports (EAB Navigate)" element={<h1>Progress Reports (EAB Navigate)</h1>} />
+                    </Routes>
+                </div>
+            </div>
+        </>
   );
 }
-
 export default Courses;
